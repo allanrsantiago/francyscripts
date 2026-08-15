94ffe7dce7848265e0b965653daa5f642d43f498
Allan Santiago <allanrodriguessantiago@gmail.com>
2026-08-15 09:21:20 -0300

    Versao 1.5.8 com telefones padr├Áes atualizados

---
+++ publicar.bat
@@ -0,0 +1,8 @@
+git init
+git remote add origin https://github.com/allanrsantiago/francyscripts.git
+git add .
+git commit -m "Versao 1.5.8 com telefones padrões atualizados"
+git branch -M main
+git push -u origin main --force
+git tag -f v1.5.8
+git push origin v1.5.8 --force
